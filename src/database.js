7927061314343864027afa4e91f162cca62e7375
SHIN-RA©神羅カンパニー <47006977+ShinraElectricAndPower@users.noreply.github.com>
2019-06-16 21:08:09 -0400

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,1 +1,31 @@
+const Sequelize = require('sequelize')
 
+const { DB } = process.env
+const database = new Sequelize(DB, { logging: false })
+
+class Postgres {
+  constructor (logger = console) {
+    this.logger = console
+  }
+
+  static get db () {
+    return database
+  }
+
+  static start () {
+    database.authenticate()
+      .then(() => this.logger.info('[POSTGRES]: Connection to database has been established successfully.'))
+      .then(() => this.logger.info('[POSTGRES]: Synchronizing database...'))
+      .then(() => database.sync()
+        .then(() => this.logger.info('[POSTGRES]: Done Synchronizing database!'))
+        .catch(error => this.logger.error(`[POSTGRES]: Error synchronizing the database: \n${error}`))
+      )
+      .catch(error => {
+        this.logger.error(`[POSTGRES]: Unable to connect to the database: \n${error}`)
+        this.logger.error(`[POSTGRES]: Try reconnecting in 5 seconds...`)
+        setTimeout(() => Postgres.start(), 5000)
+      })
+  }
+}
+
+module.exports = Postgres
